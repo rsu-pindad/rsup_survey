2e94aa14e26e8593a768661c5f6b401f553a0e30
rsu-pindad <sautomated01@gmail.com>
2024-04-19 10:46:33 +0700

    add api layananRespon resource

--- app/Http/Controllers/Api/Admin/LayananResponController.php
+++ app/Http/Controllers/Api/Admin/LayananResponController.php
@@ -11,12 +11,20 @@ use Validator;
 
 class LayananResponController extends BaseController
 {
+
+    function __contruct()
+    {
+        $this->middleware(['role:admin','permission:layanan-respon-list|layanan-respon-create|layanan-respon-edit|layanan-respon-delete'],['only' => ['index','show']]);
+        $this->middleware(['role:admin','permission:layanan-respon-create'],['only' => ['create', 'store']]);
+        $this->middleware(['role:admin','permission:layanan-respon-edit'],['only' => ['edit', 'update']]);
+        $this->middleware(['role:admin','permission:layanan-respon-delete'],['only' => ['destroy']]);
+    }
     /**
      * Display a listing of the resource.
      */
     public function index(): JsonResponse
     {
-        $layananRespon = LayananRespon::all();
+        $layananRespon = LayananRespon::paginate(50);
         return $this->sendResponse(LayananResponResource::collection($layananRespon), 'list layanan respon berhasil didapat.');
     }
 
@@ -48,9 +56,13 @@ class LayananResponController extends BaseController
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id) : JsonResponse
     {
-        //
+        $layananRespon = LayananRespon::find($id);
+        if(is_null($layananRespon)){
+            return $this->sendError('Layanan Respon tidak ditemukan.');
+        }
+        return $this->sendResponse(new LayananResponResource($layananRespon), 'show layanan respon didapat.');
     }
 
     /**
@@ -64,9 +76,20 @@ class LayananResponController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, LayananRespon $layananRespon)
     {
-        //
+        $input = $request->all();
+        $validator = Validator::make($input, [
+            'layanan_id' => 'required|numeric|exists:layanan,id',
+            'respon_id' => 'required|numeric|exists:respon,id'
+        ]);
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error', $validator->errors());
+        }
+        $layananRespon->layanan_id = $input['layanan_id'];
+        $layananRespon->respon_id = $input['respon_id'];
+        $layananRespon->save();
+        return $this->sendResponse(new LayananResponResource($layananRespon), 'layanan respon berhasil diperbarui.');
     }
 
     /**
@@ -74,6 +97,11 @@ class LayananResponController extends BaseController
      */
     public function destroy(string $id)
     {
-        //
+        $layananRespon = LayananRespon::find($id);
+        if ($layananRespon) {
+            $layananRespon->delete();
+            return $this->sendResponse('layanan respon berhasil di hapus.', 201);
+        }
+        return $this->sendError('Layanan respon tidak ditemukan');
     }
 }
