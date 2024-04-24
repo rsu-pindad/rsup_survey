6a8c7523d96ecde9f2f8a1c845f39e03be0b9b27
rsu-pindad <sautomated01@gmail.com>
2024-04-24 21:05:48 +0700

    add relation HasMany Penjamin

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Unit extends Model
 {
@@ -26,7 +27,12 @@ class Unit extends Model
         'updated_at'
     ];
 
-    // protected $casts = [
-    //     'nama_unit' => 'string'
-    // ];
+    protected $casts = [
+        'nama_unit' => 'string'
+    ];
+
+    public function penjamin() : HasMany
+    {
+        return $this->hasMany(Penjamin::class);
+    }
 }
