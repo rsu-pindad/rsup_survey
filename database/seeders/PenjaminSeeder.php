04f32b3510864d6b08378137552711cd5a56695a
rsu-pindad <sautomated01@gmail.com>
2024-04-29 08:32:36 +0700

    remove penjamin based unit, add new data factory

--- database/seeders/PenjaminSeeder.php
+++ database/seeders/PenjaminSeeder.php
@@ -2,12 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\Penjamin;
-use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Log;
-use DB;
+use App\Models\Penjamin;
 
 class PenjaminSeeder extends Seeder
 {
@@ -16,27 +13,8 @@ class PenjaminSeeder extends Seeder
      */
     public function run(): void
     {
-        $time = \Carbon\Carbon::now()->setTimezone('Asia/Jakarta');
-        try {
-            // Penjamin::truncate();
-            for ($x = 0; $x <= 20; $x++) {
-                $randomUnit = DB::table('unit')
-                    ->inRandomOrder()
-                    ->first();
-                DB::beginTransaction();
-                $penjamin = DB::table('penjamin')
-                    ->insert([
-                        'unit_id' => $randomUnit->id,
-                        'nama_penjamin' => fake()->word(),
-                        'created_at' => $time,
-                        'updated_at' => $time,
-                    ]);
-                // Log::info($penjamin);
-                // dd($randomUnit);
-                DB::commit();
-            }
-        } catch (\Throwable $th) {
-            DB::rollback();
-        }
+        Penjamin::factory()
+            ->count(4)
+            ->create();
     }
 }
