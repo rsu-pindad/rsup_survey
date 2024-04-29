371f03e3c43f65e4dfae9bb0dd93a38ad3344162
rsu-pindad <sautomated01@gmail.com>
2024-04-29 08:28:47 +0700

    add survey petugaspelayanan

--- app/Livewire/SurveyPetugasPelayanan.php
+++ app/Livewire/SurveyPetugasPelayanan.php
@@ -4,20 +4,76 @@ namespace App\Livewire;
 
 use App\Models\KaryawanProfile;
 use App\Models\LayananRespon;
+use App\Models\Respon;
+use App\Models\SurveyPelanggan;
 use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
-use App\Models\SurveyPelanggan;
 
 class SurveyPetugasPelayanan extends Component
 {
-    // #[Validate('required')]
-    // public $responSkor;
+    use LivewireAlert;
 
-    public function save($responSkor) {
-        // $this->validate();
-        // dd($responSkor);
+    #[Validate('required')]
+    public $skor;
+
+    #[Validate('required')]
+    public $nama_skor;
+
+    public function getListeners()
+    {
+        return [
+            'confirmed',
+            'cancelled'
+        ];
+    }
+
+    public function cancelled()
+    {
+        $this->skor = '';
+        $this->nama_skor = '';
+    }
+
+    public function confirmed()
+    {
         try {
+            $this->validate();
+            $store = $this->save($this->skor);
+            if ($store == true) {
+                return $this->flash('success', 'Berhasil Menilai Layanan', [
+                    'position' => 'center',
+                    'toast' => true,
+                ], '/');
+            }
+        } catch (\Throwable $th) {
+            return $this->flash('warning', 'Gagal Menilai Layanan', [
+                'position' => 'center',
+                'toast' => true,
+                'text' => $th->getMessages(),
+            ]);
+        }
+    }
+
+    public function preSave($skor)
+    {
+        $skor = Respon::where('id', $skor)->first();
+        $this->nama_skor = $skor->nama_respon;
+        $this->skor = $skor->skor_respon;
+        $this->confirm('Beri nilai ' . $this->nama_skor . ' ?', [
+            'icon' => 'question',
+            'onConfirmed' => 'confirmed',
+            'allowOutsideClick' => false,
+            'confirmButtonText' => 'Nilai',
+            'cancelButtonText' => 'Batal',
+            'onDismissed' => 'cancelled'
+        ]);
+    }
+
+    public function save($responSkor)
+    {
+        try {
+            $time = \Carbon\Carbon::now()->setTimezone('Asia/Jakarta');
             $store = new SurveyPelanggan;
             $store->karyawan_id = session()->get('karyawan_id');
             $store->penjamin_layanan_id = session()->get('penjamin_layanan_id');
@@ -25,20 +81,21 @@ class SurveyPetugasPelayanan extends Component
             $store->handphone_pelanggan = session()->get('handphone_pelanggan');
             $store->shift = 1;
             $store->nilai_skor = $responSkor;
+            $store->created_at = $time;
+            $store->updated_at = $time;
             $store->save();
-            return redirect()->route('roots-dashboard');
+            if ($store) {
+                return true;
+            }
         } catch (\Throwable $th) {
-            throw $th;
+            return $th->getMessage();
         }
     }
 
     public function render()
     {
-        // dd(session()->get('nama_pelanggan'));
         $layananKaryawan = KaryawanProfile::where('user_id', Auth::user()->id)->first();
-        // dd($layananKaryawan->toArray());
-        $repons = LayananRespon::where('layanan_id', $layananKaryawan->layanan_id)->get();
-        // dd($repons->toArray());
+        $repons = LayananRespon::distinct()->where('layanan_id', $layananKaryawan->layanan_id)->get('respon_id');
         return view('livewire.survey-petugas-pelayanan')->with([
             'petugas' => Auth::user()->name,
             'respons' => $repons,
