1da3d08e4bd1bbd075db6e99d4e6acf94ab395de
rsu-pindad <sautomated01@gmail.com>
2024-05-07 09:09:39 +0700

    add function whatsapp fonnte

--- app/Livewire/SurveyPetugasPelayanan.php
+++ app/Livewire/SurveyPetugasPelayanan.php
@@ -89,8 +89,9 @@ class SurveyPetugasPelayanan extends Component
             $store->save();
             if ($store) {
                 $this->saveSheet($responSkor, $time);
+                $this->sendWhatsapp();
                 $request->session()->forget([
-                    'penjamin_layanan_id', 'nama_pelanggan','handphone_pelanggan'
+                    'penjamin_layanan_id', 'nama_pelanggan', 'handphone_pelanggan'
                 ]);
                 return true;
             }
@@ -104,26 +105,72 @@ class SurveyPetugasPelayanan extends Component
         try {
             \Carbon\Carbon::setLocale('id');
             $karyawan = \App\Models\KaryawanProfile::with(['parentUnit', 'parentLayanan'])
-            ->where('id', session()->get('karyawan_id'))->first();
+                ->where('id', session()->get('karyawan_id'))
+                ->first();
             $penjamin = \App\Models\PenjaminLayanan::with(['parentPenjamin'])
-            ->where('penjamin_id',session()->get('penjamin_layanan_id'))->first();
+                ->where('penjamin_id', session()->get('penjamin_layanan_id'))
+                ->first();
             $timeformat = \Carbon\Carbon::parse($time)->translatedFormat('d F Y H:i');
             $sheets = Sheets::spreadsheet('1FEz14MGqe8n5UQ4voiy21nmWxYSub_eiCjuh3LLH5r0')
-            ->sheet('Sheet1')
-            ->append(
-                [
+                ->sheet('Sheet1')
+                ->append(
                     [
-                        'TGL_SURVEY' => $timeformat, 
-                        'PEGAWAI' => $karyawan->nama_karyawanprofile, 
-                        'UNIT' => $karyawan->parentUnit->nama_unit, 
-                        'PELAYANAN' => $karyawan->parentLayanan->nama_layanan,
-                        'NAMA_PASIEN' => session()->get('nama_pelanggan'),
-                        'PENJAMIN' => $penjamin->parentPenjamin->nama_penjamin,
-                        'NILAI_SURVEY_KEPUASAN' => $responSkor,
+                        [
+                            'TGL_SURVEY' => $timeformat,
+                            'PEGAWAI' => $karyawan->nama_karyawanprofile,
+                            'UNIT' => $karyawan->parentUnit->nama_unit,
+                            'PELAYANAN' => $karyawan->parentLayanan->nama_layanan,
+                            'NAMA_PASIEN' => session()->get('nama_pelanggan'),
+                            'PENJAMIN' => $penjamin->parentPenjamin->nama_penjamin,
+                            'NILAI_SURVEY_KEPUASAN' => $responSkor,
+                        ]
                     ]
-                ]);
+                );
         } catch (\Throwable $th) {
-            return $th->getMessage;
+            return $th->getMessage();
+        }
+    }
+
+    private function sendWhatsapp()
+    {
+        try {
+            $curl = curl_init();
+
+            $msg = 'Terimakasih ' . session()->get('nama_pelanggan') . ', Telah mengikuti survey';
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.fonnte.com/send',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 5,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'target' => session()->get('handphone_pelanggan'),
+                    'message' => $msg,
+                    'countryCode' => '62',  // optional
+                ),
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: ' . env('FONNTE_TOKEN')  // change TOKEN to your actual token
+                ),
+            ));
+
+            sleep(3);
+
+            $response = curl_exec($curl);
+            if (curl_errno($curl)) {
+                $error_msg = curl_error($curl);
+            }
+            curl_close($curl);
+
+            if (isset($error_msg)) {
+                echo $error_msg;
+            }
+            return $response;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
         }
     }
 
