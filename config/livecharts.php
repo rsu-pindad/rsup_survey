f062e8d6089f2eba77767ddcb561ab52bd40cff2
rsu-pindad <sautomated01@gmail.com>
2024-05-15 07:40:04 +0700

    initial chartjs

---
+++ config/livecharts.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'defaults' => [
+		'library' => 'chartjs',  // Default charting library
+		// 'poll' => 2,			// Default poll interval
+		// 'colors' => [ ],		// Array of color codes/names
+		// 'borderWidth' => 2,	// Default ChartJS borderWdith
+	],
+];
