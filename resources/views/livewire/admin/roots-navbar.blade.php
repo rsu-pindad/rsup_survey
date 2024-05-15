11a8b10adb090e478104cca7f0559a1fa7b52d7d
rsu-pindad <sautomated01@gmail.com>
2024-05-15 11:41:00 +0700

    add roots navbar

---
+++ resources/views/livewire/admin/roots-navbar.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <header class="border-bottom lh-2 py-4">
+        <div class="row justify-content-center px-2">
+        <div class="col-lg-4 col-md-3 col-xs-2 d-none d-xs-none d-md-block">
+            <img 
+            class="m-2 px-2" 
+            src="https://psurvey.pindadmedika.com/_next/image?url=%2Frsu-pindad.png&w=384&q=75" 
+            alt="logo" 
+            width="150" 
+            height="auto">
+        </div>
+        <div class="col-lg-4 col-md-6 col-xs-8 d-none d-xs-none d-md-block">
+            <div class="text-center text-body-emphasis text-decoration-none text-wrap fs-6">
+            {!! $unitAlamat !!}
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-3 col-xs-2 d-xs-block d-md-block">
+            <div class="d-flex flex-row-reverse m-2 px-2 justify-content-center">
+                <a class="btn btn-sm btn-outline-secondary mx-2 align-self-center" href="{{ route('logout') }}">Keluar</a>
+                <a class="btn btn-sm btn-outline-secondary mx-2 align-self-center" href="#"> ({{ $layanan }}) {{ $petugas }}</a>
+            </div>
+        </div>
+        </div>
+    </header>
+</div>
--- resources/views/livewire/survey-petugas-pelayanan.blade.php
+++ resources/views/livewire/survey-petugas-pelayanan.blade.php
@@ -1,24 +1,11 @@
 <div>
-    <div class="container">
-        <header class="border-bottom lh-2 py-4">
-            <div class="row d-flex justify-content-center px-2">
-                <div class="col-lg-4 col-md-3 col-xs-2 d-none d-sm-none d-md-block">
-                <img class="m-2 px-2" src="https://psurvey.pindadmedika.com/_next/image?url=%2Frsu-pindad.png&w=384&q=75" alt="logo" width="150" height="auto">
-                </div>
-                <div class="col-lg-4 col-md-6 col-xs-8 d-none d-sm-none d-md-block">
-                    <div class="text-center text-body-emphasis text-decoration-none text-wrap fs-6">
-                        {!! $unitAlamat !!}
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-3 col-xs-2 d-none d-sm-none d-md-block">
-                    <div class="d-flex flex-row-reverse m-2 px-2 justify-content-center">
-                        <a class="btn btn-sm btn-outline-secondary mx-2 align-self-center" href="{{ route('logout') }}">Keluar</a>
-                        <a class="btn btn-sm btn-outline-secondary mx-2 align-self-center" href="{{ route('sdm') }}">{{ $petugas }}</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-    </div>
+    @persist('roots-navbar')
+    <livewire:admin.roots-navbar 
+    :unitAlamat="$unitAlamat"
+    :layanan="$layanan"
+    :petugas="$petugas"
+    />
+    @endpersist
 
     <main class="container">
         <div class="container-fluid">
@@ -91,6 +78,6 @@
 @endpush
 
 @push('scripts')
-<script src="https://unpkg.com/@victoryoalli/alpinejs-timeout@1.0.0/dist/timeout.min.js" defer></script>
-<script src="https://unpkg.com/@mineru98/alpinejs-moment@1.0.3/dist/id/moment.min.js" defer></script>
+@basset('vendor/alpinejs/timeout.min.js')
+@basset('vendor/alpinejs/moment.min.js')
 @endpush
