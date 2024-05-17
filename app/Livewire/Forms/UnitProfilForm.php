7ffe1ca68e786be44e628aa0fbdae85cdb877ec3
rsu-pindad <sautomated01@gmail.com>
2024-05-17 16:16:37 +0700

    remove quill add dropzone add jodit-text-editor

--- app/Livewire/Forms/UnitProfilForm.php
+++ app/Livewire/Forms/UnitProfilForm.php
@@ -4,17 +4,26 @@ namespace App\Livewire\Forms;
 
 use App\Models\Unit;
 use App\Models\UnitProfil;
+use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Attributes\Validate;
 use Livewire\Form;
+use Livewire\WithFileUploads;
 
 class UnitProfilForm extends Form
 {
+    use WithFileUploads;
+
     // #[Locked]
     public $unitId;
 
     public $unitMainLogo;
 
     public $unitSubLogo;
+
+    public $unitMainLogoOld;
+
+    public $unitSubLogoOld;
 
     public $unitMotto;
 
@@ -28,31 +37,36 @@ class UnitProfilForm extends Form
         $this->unitId = $unit->id;
         $this->unitNama = $unit->nama_unit;
         $unitProfil = UnitProfil::where('unit_id', $this->unitId)->first();
-        $this->unitMainLogo = $unitProfil->unit_main_logo ?? '';
-        $this->unitSubLogo = $unitProfil->unit_sub_logo ?? '';
+        $this->unitMainLogoOld = $unitProfil->unit_main_logo ?? '';
+        $this->unitSubLogoOld = $unitProfil->unit_sub_logo ?? '';
         $this->unitAlamat = $unitProfil->unit_alamat ?? '';
         $this->unitMotto = $unitProfil->unit_motto ?? '';
     }
 
     public function store()
     {
+        // dd($this->all());
         try {
+            
+            $mainName = $this->unitMainLogo[0]['name'];
+            $subName = $this->unitSubLogo[0]['name'];
+            
+            $main = Storage::putFileAs('/public/basset/photos', new File($this->unitMainLogo[0]['path']), $mainName);
+            $sub = Storage::putFileAs('/public/basset/photos', new File($this->unitSubLogo[0]['path']), $subName);
+            // dd($main);
             $unitProfil = UnitProfil::updateOrCreate(
                 [
                     'unit_id' => $this->unitId,
                 ],
                 [
-                    'unit_main_logo' => $this->unitMainLogo,
-                    'unit_sub_logo' => $this->unitSubLogo,
+                    'unit_main_logo' => $mainName ?? $this->unitMainLogoOld,
+                    'unit_sub_logo' => $subName ?? $this->unitMainLogoOld,
                     'unit_alamat' => $this->unitAlamat,
                     'unit_motto' => $this->unitMotto,
                 ]
             );
-            // dd($this->all());
-            if ($unitProfil) {
-                return true;
-            }
-                return false;
+            // Storage::delete('/tmp');
+            return $unitProfil;
         } catch (\Throwable $th) {
             return $th->getMessage();
         }
