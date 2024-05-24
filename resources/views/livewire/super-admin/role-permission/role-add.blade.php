0128c086ed4fb5c0f775dce29d7f551faf6e7cea
rsu-pindad <sautomated01@gmail.com>
2024-05-24 14:25:43 +0700

    fix view layout add powergrip natural sorting change table action to icon only

--- resources/views/livewire/super-admin/role-permission/role-add.blade.php
+++ resources/views/livewire/super-admin/role-permission/role-add.blade.php
@@ -1,17 +1,29 @@
 <div>
     <form wire:submit="save">
-        <div class="mb-3">
-            <label for="namaRole" class="form-label">Nama Role</label>
-            <input wire:model="form.namaRole" 
-                type="text" 
-                class="form-control" 
-                id="namaRole" 
-                aria-describedby="namaRoleHelp"
-                placeholder="masukan nama role">
-            <div id="namaRoleHelp" class="form-text">
-                @error('namaRole') <span class="error">{{ $message }}</span> @enderror 
+        <div class="row mb-3">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="input-group has-validation">
+                    <div class="form-floating is-invalid">
+                        <input wire:model="form.namaRole" 
+                            type="text" 
+                            class="form-control" 
+                            id="namaRole" 
+                            aria-describedby="namaRoleHelp"
+                            placeholder="masukan nama role">
+                            <label for="namaRole" class="form-label">Nama Role</label>
+                    </div>
+                </div>
+                <div class="invalid-feedback">
+                    @error('namaRole') <span class="error">{{ $message }}</span> @enderror
+                </div>
             </div>
-          </div>
-          <button type="submit" class="btn btn-primary">Tambah</button>        
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">
+                    <i class="fa-solid fa-floppy-disk"></i> simpan
+                </button>
+            </div>
+        </div>
     </form>
 </div>
--- resources/views/livewire/super-admin/role-permission/role-edit.blade.php
+++ resources/views/livewire/super-admin/role-permission/role-edit.blade.php
@@ -3,32 +3,41 @@
         <livewire:admin.roots-admin /> 
     @endpersist
 
-    <main class="container-fluid p-4">
-        <div class="bg-body-tertiary p-5 rounded">
+    <main class="container-fluid px-5">
+        <div class="rounded">
             <div class="card">
                 <div class="card-body m-2">
                     <form wire:submit="edit" wire:key="{{ $this->form->id }}">
-                        <div class="mb-3 row">
-                            <label for="namaRole" class="col-sm-2 col-form-label">Nama Role</label>
-                            <div class="col-sm-10">
-                              <input 
-                                wire:model.defer="form.namaRole"
-                                type="text" 
-                                class="form-control" 
-                                id="namaRole" 
-                                value="{{ $this->form->namaRole }}">
+                        <div class="row">
+                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <div class="input-group has-validation">
+                                    <div class="form-floating is-invalid">
+                                        <input 
+                                            wire:model.defer="form.namaRole"
+                                            type="text" 
+                                            class="form-control" 
+                                            id="namaRole" 
+                                            value="{{ $this->form->namaRole }}">
+                                        <label for="namaRole" class="col-sm-2 col-form-label">Nama Role</label>
+                                    </div>
+                                    <div class="invalid-feedback">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="d-flex flex-row justify-content-center">
+                        <div class="d-flex flex-row justify-content-center flex-wrap">
                             <div class="align-self-center p-2">
-                                <a href="{{ route('root-super-admin-role') }}"
-                                    class="btn btn-outline-secondary">Batal</a>
+                                <a wire:navigate
+                                    href="{{ route('root-super-admin-role') }}"
+                                    class="btn btn-secondary">
+                                    <i class="fa-solid fa-arrow-left-long"></i> kembali
+                                </a>
                             </div>
                             <div class="align-self-center p-2">
                                 <button
                                     class="btn btn-outline-primary"
                                     type="submit">
-                                    Edit
+                                    <i class="fa-solid fa-floppy-disk"></i> perbarui
                                 </button>
                             </div>
                         </div>
