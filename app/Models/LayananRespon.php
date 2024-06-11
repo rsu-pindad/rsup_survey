59d083095abf9f37e254ac9fcc3dd9e0aa3cf70d
rsu-pindad <sautomated01@gmail.com>
2024-06-11 08:42:58 +0700

    remove soft deletes

--- app/Models/LayananRespon.php
+++ app/Models/LayananRespon.php
@@ -5,11 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LayananRespon extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'layanan_respon';
 
