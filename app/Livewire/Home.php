ed23c5775c335e29b62b47513dde85de5fb79bd6
rsu-pindad <sautomated01@gmail.com>
2024-06-20 11:17:33 +0700

    optimize using cache

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,14 +2,15 @@
 
 namespace App\Livewire;
 
-use App\Livewire\Forms\HomePenjaminForm as Form;
+use App\Models\Unit;
+use App\Models\Layanan;
+use Livewire\Component;
 use App\Models\AppSetting;
 use App\Models\KaryawanProfile;
-use App\Models\Layanan;
 use App\Models\PenjaminLayanan;
-use App\Models\Unit;
 use Livewire\Attributes\Layout;
-use Livewire\Component;
+use Illuminate\Support\Facades\Cache;
+use App\Livewire\Forms\HomePenjaminForm as Form;
 
 // use Intervention\Image\Laravel\Facades\Image;
 
@@ -27,13 +28,16 @@ class Home extends Component
     public function save()
     {
         $this->form->validate();
+
         return $this->form->store();
     }
 
     #[Layout('components.layouts.beranda')]
     public function render()
     {
-        $profile = KaryawanProfile::where('user_id', session()->get('userId'))->first();
+        $profile = Cache::remember('profile', 60, function () {
+            return KaryawanProfile::with(['parentUnit','parentLayanan'])->where('user_id', session()->get('userId'))->first();
+        });
         if ($profile == null) {
             return <<<HTML
                 <div>
@@ -42,7 +46,9 @@ class Home extends Component
                 </div>
                 HTML;
         }
-        $layanan = Layanan::findOrFail($profile->layanan_id);
+        $layanan = Cache::remember('layanan', 60, function () use ($profile) {
+            return Layanan::find($profile->layanan_id);
+        });
         if ($layanan == null) {
             return <<<HTML
                 <div>
@@ -51,24 +57,35 @@ class Home extends Component
                 </div>
                 HTML;
         }
-        $penjaminLayanan = PenjaminLayanan::distinct()
-            ->where('layanan_id', $layanan->id)
-            ->get('penjamin_id');
-        $unit = Unit::with('unitProfil')->find($profile->parentUnit->id);
-        $appSetting = AppSetting::get()->last();
+        // $penjaminLayanan = Cache::remember('penjaminLayanan', 60, function () use ($layanan) {
+        //     return PenjaminLayanan::distinct()
+        //                ->where('layanan_id', $layanan->id)
+        //                ->get('penjamin_id');
+        // });
+        $unit = Cache::remember('unitProfile', 60, function () use ($profile) {
+            return Unit::with('unitProfil')->find($profile->parentUnit->id);
+        });
+        $appSetting = Cache::remember('appSetting', 60, function () {
+            return AppSetting::get()->last();
+        });
         session()->put('multiPenilaian', $profile->parentUnit->multi_penilaian);
         session()->put('userUnitId', $profile->parentUnit->id);
         session()->put('userLayananId', $profile->parentLayanan->id);
         session()->put('userLayananMulti', $profile->parentLayanan->multi_layanan);
+
         return view('livewire.home')->with([
-            'petugas' => $profile->nama_karyawanprofile,
-            'layanan' => $profile->parentLayanan->nama_layanan,
-            'unitNama' => $profile->parentUnit->nama_unit,
+            'petugas'    => $profile->nama_karyawanprofile,
+            'layanan'    => $profile->parentLayanan->nama_layanan,
+            'unitNama'   => $profile->parentUnit->nama_unit,
             'unitAlamat' => $unit->unitProfil->unit_alamat ?? $appSetting->initial_alamat_text,
-            'penjamin' => $penjaminLayanan,
-            'mainLogo' => $unit->unitProfil->unit_main_logo ?? 'settings/' . $appSetting->initial_body_logo,
-            'subLogo' => $unit->unitProfil->unit_sub_logo ?? 'settings/' . $appSetting->initial_header_logo,
-            'unitMoto' => $unit->unitProfil->unit_motto ?? $appSetting->initial_moto_text,
+            'penjamin'   => Cache::remember('penjaminLayanan', 60, function () use ($layanan) {
+                return PenjaminLayanan::distinct()
+                           ->where('layanan_id', $layanan->id)
+                           ->get('penjamin_id');
+            }),
+            'mainLogo'   => $unit->unitProfil->unit_main_logo ?? 'settings/' . $appSetting->initial_body_logo,
+            'subLogo'    => $unit->unitProfil->unit_sub_logo ?? 'settings/' . $appSetting->initial_header_logo,
+            'unitMoto'   => $unit->unitProfil->unit_motto ?? $appSetting->initial_moto_text,
         ]);
     }
 }
