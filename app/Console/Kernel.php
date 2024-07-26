e608dc0de7f1f55d6e76858a5bfea4b86c074f8a
rsu-pindad <sautomated01@gmail.com>
2024-07-26 13:05:38 +0700

    change to hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('queue:work --rest=2 --stop-when-empty')->hourly();
     }
 
     /**
