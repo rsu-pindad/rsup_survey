13d83aa6079634e7f63b301c0cd3aa71e9190761
rsu-pindad <sautomated01@gmail.com>
2024-07-29 15:42:17 +0700

    (testing) uncomplete migration

---
+++ database/migrations/2024_07_29_113204_remove_unique_phone_from_survey_pelanggan_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('survey_pelanggan');
+        Schema::create('survey_pelanggan', function (Blueprint $table) {
+            // $table->dropUnique('survey_pelanggan_handphone_pelanggan_unique');
+            // $table->string('nilai_skor')->nullable()->change();
+            $table->id();
+            $table->unsignedBigInteger('karyawan_id');
+            $table->unsignedBigInteger('penjamin_id');
+            $table->unsignedBigInteger('layanan_id');
+            $table->string('nama_pelanggan')->nullable();
+            $table->string('handphone_pelanggan')->nullable();
+            $table->enum('shift', ['pagi','siang','malam']);
+            $table->string('nilai_skor')->nullable();
+            $table->dateTimeTz('survey_masuk', $precision = 0);
+            $table->foreign('karyawan_id')->references('id')->on('karyawan')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreign('penjamin_id')->references('id')->on('penjamin')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreign('layanan_id')->references('id')->on('layanan')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Schema::dropIfExists('survey_pelanggan');
+        // Schema::table('survey_pelanggan', function (Blueprint $table) {
+        //     // $table->addUnique('survey_pelanggan_handphone_pelanggan_unique');
+        //     // $table->unique('handphone_pelanggan','survey_pelanggan');
+        //     // $table->tinyInteger('nilai_skor')->default(0)->change();
+        // });
+    }
+};
