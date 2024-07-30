6a90e4497590f1c7a2df529391a06cb7239d9dad
rsu-pindad <sautomated01@gmail.com>
2024-07-30 11:01:54 +0700

    fix missing class

--- app/Jobs/InsertSurveyPelangganMulti.php
+++ app/Jobs/InsertSurveyPelangganMulti.php
@@ -7,8 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-// use App\Models\SurveyPelanggan;
-use Illuminate\Support\Facades\DB;
+use App\Models\SurveyPelanggan;
+// use Illuminate\Support\Facades\DB;
 
 class InsertSurveyPelangganMulti implements ShouldQueue
 {
